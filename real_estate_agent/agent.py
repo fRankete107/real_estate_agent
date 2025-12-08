e2a7fdaee2370d8b4ebd2b72af3dd00553825b7f
FStevenson91 <felstefer28791@gmail.com>
2025-12-08 15:17:00 -0300

    callback imports

--- real_estate_agent/agent.py
+++ real_estate_agent/agent.py
@@ -2,6 +2,7 @@ from google.adk.agents import Agent
 from google.genai import types
 from .prompt import agent_prompt
 from .tools import detect_location, create_contact, get_contact, update_contact, list_contacts
+from .callback import before_model_callback
 
 
 real_estate_agent = Agent(
@@ -9,6 +10,7 @@ real_estate_agent = Agent(
     model="gemini-2.0-flash",
     description="real estate agent that qualifies leads using the BANT criteria in a few steps.",
     instruction=agent_prompt,
-     tools =[detect_location, create_contact, get_contact, update_contact, list_contacts]
+    tools =[detect_location, create_contact, get_contact, update_contact, list_contacts],
+    before_model_callback=before_model_callback
     #  generate_content_config=types.GenerateContentConfig(temperature=0.5)
 )
